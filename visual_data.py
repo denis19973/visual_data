f1e3a1db22ac0d361348d92a09ababf735681416
denis19973 <oxingteam@gmail.com>
2017-02-13 21:58:53 +0200

    working api

--- visual_data.py
+++ visual_data.py
@@ -1,12 +1,33 @@
 from flask import Flask
+from pymongo import MongoClient
+from flask import jsonify
+
 
 app = Flask(__name__)
 
+def get_db():
+    client = MongoClient('localhost:27017')
+    db = client.flask_test
+    return db
 
-@app.route('/')
+
+@app.route('/get_data', methods=['GET'])
 def hello_world():
-    return 'Hello World!'
+    data = {}
+
+    for elem in db.world_bank.find():
+        # Structure of data
+        # {'project_name': {'country_name': 'UK', 'lendprojectcost': 1000}}
+        data[elem['project_name']] = dict(country_name=elem['countryname'], lendprojectcost=elem['lendprojectcost'])
+
+    return jsonify(data)
 
 
 if __name__ == '__main__':
+    db = get_db()
     app.run()
+
+
+
+
+
